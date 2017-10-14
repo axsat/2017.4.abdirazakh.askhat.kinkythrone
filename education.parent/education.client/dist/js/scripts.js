1b88e8ef493a8dd8e61fd88cc6946683e26d61ff
axsat <abdirazaxm@gmail.com>
2017-10-14 10:46:47 +0600

    First Commit

--- education.parent/education.client/dist/js/scripts.js
+++ education.parent/education.client/dist/js/scripts.js
@@ -7,15 +7,20 @@ angular.module('MyApp', ['ui.router'])
             .state('any', {
                 url: '/',
                 templateUrl: '../views/log.html',
-                controller: function($rootScope,$state){
+                controller: function($scope,$rootScope,$state,$http){
                     console.log("OKKK");
+                    $scope.val = "lmlm";
+                    $http({
+                        method: 'GET',
+                        url: 'http://localhost:1314/education/api/client/list'
+                    }).then(function successCallback(response) {
+                        // $scope.model = response.data.clientInfoList[0].name;
+                        console.log(response.data.clientInfoList[0]);
+                        console.log(response.data.clientInfoList[1]);
+                    }, function errorCallback(response) {
+                        // called asynchronously if an error occurs
+                        // or server returns response with an error status.
+                    });
                 }
             })
-            .state('reg', {
-            url: '/',
-            templateUrl: '../views/reg.html',
-            controller:function($rootScope,$state){
-                console.log("OKKK");
-            }
-        });
     }]);
